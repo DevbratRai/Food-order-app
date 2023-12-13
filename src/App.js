a46d54e96af1733f75a54a47ec21c2730d7991f4
Dev <raidb2002@gmail.com>
2023-12-13 09:55:13 +0530

    context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
+
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -9,17 +11,19 @@ function App() {
   const showCartHandler = () => {
     setCartIsShown(true);
   };
+
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
+
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
