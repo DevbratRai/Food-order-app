a8937deb75c1c4bbbb085b9af43552576562794f
Dev <raidb2002@gmail.com>
2024-02-08 09:02:40 +0530

    edit

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
 import ProfileForm from "./ProfileForm";
+import axios from "axios";
 
 const Profile = () => {
   const [showProfileForm, setShowProfileForm] = useState(false);
 
   const handleForm = () => {
     setShowProfileForm(true);
+  };
+
+  const handleProfileCancel = () => {
+    setShowProfileForm(false);
+  };
+
+  const handleFormUpdate = ({ fullName, profilePhotoUrl }) => {
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAP1-eVHrp1KJc8tPZNQOeQQDpl_LdLxdE",
+        {
+          fullName: fullName,
+          profilePhotoUrl: profilePhotoUrl,
+        }
+      )
+      .then((response) => {
+        // Handle successful response
+        console.log("User details updated successfully");
+        // onCancel(); // Close the form
+      })
+      .catch((error) => {
+        // Handle error
+        console.error("Error updating user details", error);
+      });
   };
   return (
     <div>
@@ -16,7 +41,10 @@ const Profile = () => {
       </p>
       {showProfileForm && (
         <div>
-          <ProfileForm />
+          <ProfileForm
+            onCancel={handleProfileCancel}
+            onUpdate={handleFormUpdate}
+          />
         </div>
       )}
     </div>
