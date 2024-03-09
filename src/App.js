02a6fb6ecd70f91ecd3a922eb8d851ae0937a8b4
Dev <raidb2002@gmail.com>
2024-03-09 09:12:41 +0530

    forgot password

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import Root from "./Root";
 import Login from "./components/Login";
 import Profile from "./components/Profile/Profile";
 import Signup from "./components/SignUp";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
 function App() {
   return (
     <>
       {/* <Signup />
       <Login /> */}
-      <Profile />
+      {/* <Profile /> */}
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,18 @@ import React from "react";
 import { useState, useRef } from "react";
 import classes from "./SignUp.module.css";
 import axios from "axios";
+import { Navigate } from "react-router-dom";
+import SignUp from "./SignUp";
 
 const Login = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+  const [islogin, setIslogin] = useState(true);
   const [error, setError] = useState(null);
+  const switchAuthModeHandler = () => {
+    setIslogin((prevState) => !prevState);
+  };
+
   const handleLogin = (event) => {
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
@@ -26,6 +33,7 @@ const Login = () => {
           console.log("User has successfully logged in");
 
           localStorage.setItem("userToken", response.data.idToken);
+          return <Navigate to={"/profile"} />;
         }
       })
       .catch((error) => {
@@ -34,32 +42,39 @@ const Login = () => {
   };
   return (
     <div className={classes.container}>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        {error && <p className={classes.error}>{error}</p>}
-        <div className={classes.control}>
-          <label>Email:</label>
-          <input type="email" id="email" required ref={emailInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label>Password:</label>
-          <input
-            type="password"
-            id="password"
-            required
-            ref={passwordInputRef}
-          />
-        </div>
+      {islogin && (
+        <>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            {error && <p className={classes.error}>{error}</p>}
+            <div className={classes.control}>
+              <label>Email:</label>
+              <input type="email" id="email" required ref={emailInputRef} />
+            </div>
+            <div className={classes.control}>
+              <label>Password:</label>
+              <input
+                type="password"
+                id="password"
+                required
+                ref={passwordInputRef}
+              />
+            </div>
 
-        <div className={classes.actions}>
-          <button>Login</button>
-          <button>Forgot Password</button>
-
-          <button type="button" className={classes.toggle}>
-            Don't Have an account ? SignUp
-          </button>
-        </div>
-      </form>
+            <div className={classes.actions}>
+              <button>Login</button>
+              <button>Forgot Password</button>
+            </div>
+          </form>
+        </>
+      )}
+      <button
+        type="button"
+        className={classes.toggle}
+        onClick={switchAuthModeHandler}
+      >
+        {islogin ? "Don't Have an account ? SignUp" : <SignUp />}
+      </button>
     </div>
   );
 };
